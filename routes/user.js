d9b4abf6bee21c82fbcd3c4be352548e674cd82c
Raj Singh <rajsingh.19.vns@gmail.com>
2024-12-20 16:53:49 +0530

    Chandes folder structure

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const dotenv = require("dotenv");
-
+// Importing controllers
 const registerHandler = require("../controller/registerController");
 const loginHandler = require("../controller/loginController");
+// Configuring dotenv to load environment variables
+dotenv.config();        // Load .env file variables into process.env
+// Defining routes
+router.post('/register', registerHandler);      // POST route for user registration, handled by registerHandler
+router.post('/login', loginHandler);            // POST route for user login, handled by loginHandler
 
-dotenv.config();
 
-router.post('/register', registerHandler);
-
-router.post('/login', loginHandler);
-
+// Exporting the router to use in other parts of the application
 module.exports = router;
