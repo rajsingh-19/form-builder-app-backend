bf0e84377a494fd666e30d988e487c172f8678c2
Raj Singh <rajsingh.19.vns@gmail.com>
2024-12-22 22:44:28 +0530

    implemented dotenv connfig

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require('cors');
+const cors = require("cors");
 const userRoute = require("./routes/user");
 const connectMongoDB = require("./config/dbconfig");
 
-const app = express();
-connectMongoDB();
-const PORT = process.env.PORT || 4120;
+const app = express();          // Creating an Express application instance
+connectMongoDB();               // Calling the function to connect to MongoDB
+dotenv.config();                // Loading environment variables from .env file
 
-app.use(cors());
-app.use(express.json());
+app.use(cors());                // Enabling Cross-Origin Resource Sharing
+app.use(express.json());        // Middleware to parse JSON bodies in requests
 
+const PORT = process.env.PORT || 4120;      // Defining the port for the server, defaulting to 4120 if not set in .env
+// Defining a route for user-related operations, prefixed with /api/use
 app.use('/api/user', userRoute);
-
+// Defining a simple root route for the application
 app.get('/', (req, res) => {
     res.send("Hello");
 });
-
 
 //          wait for the db connection before starting the server
 mongoose.connection.once('open', () => {
