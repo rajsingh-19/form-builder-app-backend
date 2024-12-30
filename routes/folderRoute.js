a03ab163aebbff0ccf92100e2d0fbef42f23f86a
Raj Singh <rajsingh.19.vns@gmail.com>
2024-12-31 00:13:20 +0530

    Updated the folder routes for fixing the bug of folder creation

--- routes/folderRoute.js
+++ routes/folderRoute.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const { createFolder, addFormToFolder, getFolders, deleteFolder } = require('../controller/folderController');
-const checkAccess = require('../middleware/checkAccess');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 // Route for creating folders
-router.post('/:dashboardId/folder', authMiddleware, checkAccess('edit'), createFolder);
+router.post('/:dashboardId/folder', authMiddleware, createFolder);
 
 // Route for adding a form to an existing folder
 // This will use the URL params for dashboardId, folderId, and formId
-router.post('/:dashboardId/folder/:folderId/form', authMiddleware, checkAccess('edit'), addFormToFolder);
+router.post('/:dashboardId/folder/:folderId/form', authMiddleware, addFormToFolder);
 
 // Route for fetching all folders of a dashboard
-router.get('/:dashboardId/folders', authMiddleware, checkAccess('view'), getFolders);
+router.get('/:dashboardId/folders', authMiddleware, getFolders);
 
 // Route for deleting a folder
-router.delete('/:dashboardId/folder/:folderId', authMiddleware, checkAccess('edit'), deleteFolder);
+router.delete('/:dashboardId/folder/:folderId', authMiddleware, deleteFolder);
 
 module.exports = router;
