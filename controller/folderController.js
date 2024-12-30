254e66719e2b979ffedfca12b7413ac025f04fe7
Raj Singh <rajsingh.19.vns@gmail.com>
2024-12-31 01:27:30 +0530

    fixing bug

--- controller/folderController.js
+++ controller/folderController.js
@@ -35,7 +35,7 @@ const createFolder = async (req, res) => {
 
         // Return the created folder, which will include the MongoDB-generated _id as a string
         const createdFolder = dashboard.folders[dashboard.folders.length - 1];
-        res.status(201).json(createFolder);
+        res.status(201).json(createdFolder);
     } catch (error) {
         console.error('Error creating folder:', error);
         res.status(500).json({ message: 'Error creating folder', error:"error.message" });
